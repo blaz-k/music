e0cef4d03a8e5269285e1a57ebfeef621f13f555
Blaz <blazyy@gmail.com>
2022-01-24 12:00:05 +0100

    vee-validate changed version, vee-validate/rules installed; registration form name corrected

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -81,15 +81,18 @@
             </button>
           </form>
           <!-- Registration Form -->
-          <form v-show="tab === 'register'">
+          <vee-form v-show="tab === 'register'" :validation-schema="schema">
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
-              <input
+              <vee-field
                 type="text"
+                name="name"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
                 placeholder="Enter Name"
               />
+
+              <ErrorMessage class="text-red-600" name="name" />
             </div>
             <!-- Email -->
             <div class="mb-3">
@@ -148,7 +151,7 @@
             >
               Submit
             </button>
-          </form>
+          </vee-form>
         </div>
       </div>
     </div>
@@ -163,6 +166,15 @@ export default {
   data() {
     return {
       tab: "login",
+      schema: {
+        name: "required",
+        email: "",
+        age: "",
+        password: "",
+        confirm_password: "",
+        country: "",
+        tos: "",
+      },
     };
   },
   computed: {
--- src/includes/validation.js
+++ src/includes/validation.js
@@ -1,8 +1,24 @@
-import { Form as VeeForm, Field as VeeField } from "vee-validate";
+import { Form as VeeForm, Field as VeeField, defineRule, ErrorMessage } from "vee-validate"; // eslint-disable-line
+
+import { required } from "@vee-validate/rules";
 
 export default {
+  // components: {
+  //   VeeField,
+  //   VeeForm,
+  // },
+  // methods: {
+  //   // Validator function
+  //   isRequired(value) {
+  //     return value ? true : "This field is required";
+  //   },
+  // },
+
   install(app) {
     app.component("VeeForm", VeeForm);
     app.component("VeeField", VeeField);
+    app.component("ErrorMessage", ErrorMessage);
+
+    defineRule("required", required);
   },
 };
