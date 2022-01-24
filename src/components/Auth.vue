42d9499d9d7fa55ea597978678e7b92e92e5483e
Blaz <blazyy@gmail.com>
2022-01-24 14:00:15 +0100

    custom error messages created

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -81,7 +81,12 @@
             </button>
           </form>
           <!-- Registration Form -->
-          <vee-form v-show="tab === 'register'" :validation-schema="schema" @submit="register">
+          <vee-form
+            v-show="tab === 'register'"
+            :validation-schema="schema"
+            @submit="register"
+            :initial-values="userData"
+          >
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
@@ -119,14 +124,18 @@
             <!-- Password -->
             <div class="mb-3">
               <label class="inline-block mb-2">Password</label>
-              <vee-field
-                name="password"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Password"
-                type="password"
-              />
+              <vee-field name="password" :bails="false" v-slot="{ field, errors }">
+                <input
+                  class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+                  placeholder="Password"
+                  type="password"
+                  v-bind="field"
+                />
+                <div class="text-red-600" v-for="error in errors" :key="error">
+                  {{ error }}
+                </div>
+              </vee-field>
             </div>
-            <ErrorMessage class="text-red-600" name="password" />
 
             <!-- Confirm Password -->
             <div class="mb-3">
@@ -191,9 +200,12 @@ export default {
         email: "required|email|min:4|max:100",
         age: "required|min_value:18|max_value:130",
         password: "required|min:3|max:100",
-        confirm_password: "confirmed:@password",
-        country: "required|excluded:Slovenia",
-        tos: "required",
+        confirm_password: "passwords_mismatch:@password",
+        country: "required|country_excluded:Slovenia",
+        tos: "tos",
+      },
+      userData: {
+        country: "USA",
       },
     };
   },
