445a316bb114a3e66b35755e855240d4bc94bc42
Blaz <blazyy@gmail.com>
2022-01-25 14:30:12 +0100

    manage.vue added; router corrected; redirecting to Home after Logout

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,45 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "@/views/Home.vue";
+import About from "@/views/About.vue";
+import Manage from "@/views/Manage.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
+    path: "/",
+    name: "Home",
     component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: "/about",
+    name: "About",
+    component: About,
+  },
+  {
+    path: "/manage-music",
+    name: "Manage",
+    component: Manage,
+    beforeEnter: (to, from, next) => {
+      next();
+    },
+  },
+  {
+    path: "/manage",
+    redirect: { name: "Manage" },
+  },
+
+  {
+    path: "/:catchAll(.*)*",
+    redirect: { name: "Home" },
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+  linkExactActiveClass: "text-yellow-500",
+});
+router.beforeEach((to, from, next) => {
+  next();
 });
 
 export default router;
