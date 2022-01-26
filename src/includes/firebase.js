0cb085f0af839bfbf214a334f0bae27e0943bee1
Blaz <blazyy@gmail.com>
2022-01-26 11:46:12 +0100

    uploading only mp3 files on page

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -3,6 +3,7 @@ import { getAuth } from "firebase/auth";
 // import "firebase/firestore";
 // import "firebase/compat/firestore";
 import { getFirestore, collection } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDE0FBPYiTJQ287Jxni3NXpLba0kyByLxg",
@@ -17,10 +18,13 @@ const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
 const db = getFirestore(app);
+const storage = getStorage(app);
+// const byteFiles = uploadBytes();
 
 const usersCollection = collection(db, "users");
 
 // const test = addDoc(usersCollection)
 
 // eslint-disable-next-line import/prefer-default-export
-export { auth, db, usersCollection };
+// eslint-disable-next-line object-curly-newline
+export { auth, db, usersCollection, storage };
